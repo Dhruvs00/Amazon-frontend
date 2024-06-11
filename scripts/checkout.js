709ff97704a4597895963fe6679c6ffa934659ce
dhruvs <dhruvsaini847@gmail.com>
2024-06-11 09:36:09 +0530

    Fixed radio selector

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem)=>{
       ${matchingproduct.name}
     </div>
     <div class="product-price">
-      ${matchingproduct.priceCents}
+      ${(matchingproduct.priceCents/100).toFixed(2)}
     </div>
     <div class="product-quantity">
       <span>
@@ -33,7 +33,52 @@ cart.forEach((cartItem)=>{
         Delete
       </span>
     </div>
-  </div>`
+  </div>
+   <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="${productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked class="delivery-option-input"
+                    name="${productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="${productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`
   cartSummaryHtml+=prodhtml;
   console.log(cartSummaryHtml);
 }) 
